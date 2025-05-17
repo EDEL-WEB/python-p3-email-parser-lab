05295ff5cd1ee75a68eb0933cafa5fc524fefb45
edel <edel.omondi@student.moringaschool.com>
2025-05-17 21:50:20 +0300

    lab complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,34 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, strArg:str):
+        self.string = strArg
+
+    def parse(self):
+        matched = []
+        emailPattern = r"[a-z.]+[a-z0-9]+@[a-z]+\.[a-z+]"
+        mailList = self.separate()
+        email = re.compile(emailPattern)
+        for mail in mailList:
+            if email.match(mail):
+                matched.append(mail)
+
+        matched.sort()  
+        return matched
+    
+    def separate(self):
+        mails = []
+        pos = 0
+        for i in range(len(self.string)):
+            if self.string[i] == " " or self.string[i] == ",":
+                mail = self.string[pos:i]
+                pos = i + 1
+                mails.append(mail)
+            elif  i == len(self.string) - 1:
+                mail = self.string[pos:i+1]
+                mails.append(mail)
+        return mails
+
+parser = EmailAddressParser("talk@talk.com john.jones@flatironschool.com alexa@amazon.com")
+parser.parse()
